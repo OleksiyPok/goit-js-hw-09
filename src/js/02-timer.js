fa0c0fff4e12aaa2e5ec4f5f912010cd7f30a457
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-02-16 21:33:59 +0100

    added onClick

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,8 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
 const datetime = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button');
 
@@ -6,53 +11,64 @@ const hoursSpan = document.querySelector('[data-hours]');
 const minutesSpan = document.querySelector('[data-minutes]');
 const secondsSpan = document.querySelector('[data-seconds]');
 
-const dateEnd = new Date('2023-02-15 15:02:00.000');
-const timerID = setInterval(onTimerTick, 1000);
+const options = {
+  minDate: 'today',
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: Date.now(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    checkDate(selectedDates[0]);
+  },
+};
 
-function onTimerTick() {
-  const diffTime = calculateDTime(dateEnd - Date.now());
-  // const diffTime = convertMs(dateEnd - Date.now());
-  showTime(diffTime);
+startBtn.addEventListener('click', onClickStart);
+startBtn.disabled = true;
+
+const flatpickrObj = flatpickr(datetime, options);
+
+function checkDate(selectedDate) {
+  if (selectedDate <= Date.now()) {
+    alert('Please choose a date in the future');
+    return;
+  }
+  startBtn.disabled = false;
 }
 
-function calculateDTime(dms) {
-  const dSeconds = Math.floor((dms / 1000) % 60);
-  const dMinutes = Math.floor((dms / (1000 * 60)) % 60);
-  const dHours = Math.floor((dms / (1000 * 60 * 60)) % 24);
-  const dDays = Math.floor(dms / (1000 * 60 * 60 * 24));
+function onClickStart() {
+  const dateEnd = flatpickrObj.selectedDates[0].getTime();
+  if (dateEnd <= Date.now()) {
+    clearInterval(timerID);
+    return;
+  }
+  startBtn.disabled = true;
+  const timerID = setInterval(onTimerTick, 1000);
 
-  if (dms <= 0) return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  function onTimerTick() {
+    const diffTime = calculateDTime(dateEnd - Date.now());
+    showTime(diffTime);
+  }
 
-  return {
-    days: dDays,
-    hours: dHours,
-    minutes: dMinutes,
-    seconds: dSeconds,
-  };
-}
+  function calculateDTime(dms) {
+    const dSeconds = Math.floor((dms / 1000) % 60);
+    const dMinutes = Math.floor((dms / (1000 * 60)) % 60);
+    const dHours = Math.floor((dms / (1000 * 60 * 60)) % 24);
+    const dDays = Math.floor(dms / (1000 * 60 * 60 * 24));
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    if (dms <= 0) return { days: 0, hours: 0, minutes: 0, seconds: 0 };
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    return {
+      days: dDays,
+      hours: dHours,
+      minutes: dMinutes,
+      seconds: dSeconds,
+    };
+  }
 
-  return { days, hours, minutes, seconds };
-}
-
-function showTime({ days, hours, minutes, seconds }) {
-  daysSpan.textContent = days;
-  hoursSpan.textContent = String(hours).padStart(2, '0');
-  minutesSpan.textContent = String(minutes).padStart(2, '0');
-  secondsSpan.textContent = String(seconds).padStart(2, '0');
+  function showTime({ days, hours, minutes, seconds }) {
+    daysSpan.textContent = days;
+    hoursSpan.textContent = String(hours).padStart(2, '0');
+    minutesSpan.textContent = String(minutes).padStart(2, '0');
+    secondsSpan.textContent = String(seconds).padStart(2, '0');
+  }
 }
